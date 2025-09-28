1c59c3e9aa85d13ee55b05e567311094a08976b7
Abdullah Al Marzan <marzansust16@gmail.com>
2025-09-28 09:14:03 +0600

    update

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
         /*{ image: 'media/home/slider/Python Plot.jpg', caption: 'Data Analysis with Python' },*/
         /*{ image: 'media/home/slider/Leminar Airflow.jpg', caption: 'Laminar Airflow Hood' },*/
         { image: 'media/home/slider/Biochemical Assay.jpg?v=1', caption: 'Biochemical Assay' },
-        { image: 'media/home/slider/Research Activities.jpg?v=1', caption: 'Research Activities' }
+        { image: 'media/home/slider/Research Activities.jpg?v=2', caption: 'Research Activities' }
         /*{ image: 'media/home/slider/Research Activities of Abdullah Al Marzan..jpg?v=1', caption: 'Research Interests' },*/
         /*{ image: 'media/home/slider/Weekly Meeting.jpg', caption: 'Research Webinar' },*/
         /*{ image: 'media/home/slider/Research Meeting with University team.jpg', caption: 'Research Meeting with University Team' }*/
